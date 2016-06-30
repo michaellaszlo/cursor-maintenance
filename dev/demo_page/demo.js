6b14446540c9762bd4beffa8cb3696467acda278
Michael Laszlo <git@michaellaszlo.com>
2016-06-29 23:35:08 -0400

    Toggle style and formatting on click

--- dev/demo_page/demo.js
+++ dev/demo_page/demo.js
@@ -1,4 +1,10 @@
 var CursorMaintenanceDemo = (function () {
+  var messages = {
+        formatting: {
+          off: '<span class="icon">&#x2610;</span>formatting disabled',
+          on: '<span class="icon">&#x2611;</span>formatting on'
+        }
+      };
 
   function commatizeValidator(text) {
     return /^[0-9,]*$/.test(text);
@@ -10,9 +16,15 @@ var CursorMaintenanceDemo = (function () {
         toggleBox = document.createElement('div'),
         i;
     function respond() {
-      var text = input.value,
-          cursor = input.selectionStart,
+      var text,
+          cursor,
           formatted;
+      console.log(input.status.formatting);
+      if (!input.status.formatting) {
+        return;
+      }
+      text = input.value;
+      cursor = input.selectionStart;
       if (text === previous.text) {
         previous.cursor = cursor;
         return;
@@ -28,7 +40,20 @@ var CursorMaintenanceDemo = (function () {
     for (i = 0; i < eventNames.length; ++i) {
       input.addEventListener(eventNames[i], respond);
     }
-    toggleBox.innerHTML = '<span class="toggle active">formatting on</span>';
+    input.status = { formatting: false };
+    toggleBox.className = 'toggle';
+    toggleBox.onclick = function () {
+      if (!input.status.formatting) {
+        input.status.formatting = true;
+        this.className += ' active';
+        toggleBox.innerHTML = messages.formatting.on;
+      } else {
+        input.status.formatting = false;
+        this.className = this.className.replace(/\s*active\s*/, ' ');
+        toggleBox.innerHTML = messages.formatting.off;
+      }
+    };
+    toggleBox.click();
     input.parentNode.insertBefore(toggleBox, input);
   }
 
