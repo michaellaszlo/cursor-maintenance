d2d3e0eda7d1b208fa2aa9e290b6b85aeecebeac
Michael Laszlo <git@michaellaszlo.com>
2016-07-10 23:16:14 -0400

    Keep note container as is for expander

--- dev/note_expander/note_expander.js
+++ dev/note_expander/note_expander.js
@@ -1,52 +1,28 @@
 var NoteExpander = (function () {
   'use strict';
 
-  // Collapse all notes. Add an expander widget to each note section.
-  function makeExpanderAction(container, columns, content) {
-    var snippet = content.snippet,
-        parts = [],
-        n = snippet.length,
-        x, i;
-    for (i = 0; i < n; ++i) {
-      x = Math.floor(255 * i / n);
-      parts.push('<span style="color: rgb(' + x + ', ' + x + ', ' + x + ')">' +
-          snippet.charAt(i) + '</span>');
-    }
-    snippet = parts.join('');
+  function makeExpanderAction(button, content) {
     return function () {
-      if (container.className.indexOf('expander-collapsed') == -1) {
-        container.className += ' expander-collapsed';
-        columns.content.innerHTML = snippet;
+      console.log(content.className);
+      if (content.className.indexOf('expander-collapsed') == -1) {
+        content.className += ' expander-collapsed';
       } else {
-        container.className = container.className.replace(
+        content.className = content.className.replace(
             /\s+expander-collapsed/, '');
-        columns.content.innerHTML = content.full;
       }
     }
   }
 
-  function enable(container, doNotCollapse) {
-    var columns, content, snippet;
-    columns = {
-      button: document.createElement('div'),
-      content: document.createElement('div')
-    };
-    columns.button.className = 'expander-button';
-    columns.button.innerHTML = '<span class="expander-icon">&#x22ef;</span>';
-    columns.content.className = 'expander-content';
-    content = {
-      full: container.innerHTML
-    };
-    snippet = content.full.replace(/\s*<p>\s*/g, '')
-    snippet = '<p>' + snippet.substring(0, snippet.indexOf(' ', 20));
-    content.snippet = snippet;
-    container.innerHTML = '';
-    container.insertBefore(columns.content, container.firstChild);
-    container.insertBefore(columns.button, container.firstChild);
-    columns.button.onclick = makeExpanderAction(container, columns, content);
-    columns.button.click();
+  function enable(content, doNotCollapse) {
+    var button = document.createElement('div');
+    button.className = 'expander-button';
+    button.innerHTML = '<span class="expander-icon">&#x22ef;</span>';
+    content.className = 'expander-content';
+    content.parentNode.insertBefore(button, content);
+    button.onclick = makeExpanderAction(button, content);
+    button.click();
     if (doNotCollapse) {
-      columns.button.click();
+      button.click();
     }
   }
 
